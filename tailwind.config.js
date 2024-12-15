d65717dfb1fcbefd8b4155a6fc3891892b44a6fd
aa096 <126397865+aa096@users.noreply.github.com>
2024-12-15 20:08:52 +0100

    moved file

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ export default {
         barlow: ["Barlow", "sans-serif"],
       },
       backgroundImage: {
-        "portfolio": "url('src/assets/images/portfolio.png')",
+        "portfolio": "url('public/assets/images/portfolio.png')",
       },
       screens: {
         'sl': "880px"
