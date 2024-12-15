259910e45a752cc29a2de681467d2ade9fa44b39
aa096 <126397865+aa096@users.noreply.github.com>
2024-12-15 19:53:12 +0100

    updates

--- src/components/templates/HolidazeInfo.jsx
+++ src/components/templates/HolidazeInfo.jsx
@@ -2,35 +2,35 @@ import IconButton from "../iconButton";
 import {faGlobe} from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
-function NobleOakInfo() {
+function HolidazeInfo() {
   return (
       <section className="flex justify-center">
         <div className="bg-white rounded-xl flex place-items-center z-50 flex-col p-10 mt-[160px] sm:mt-[200px] md:mt-[240px] sl:mt-[280px] lg:ml-[450px] lg:mt-[100px] xl:ml-[550px] xl:mt-[120px] text-secondary max-w-[1200px] w-full">
           <div className="flex flex-row gap-6 justify-center group">
             <img
-              src="src/assets/images/noble.png"
-              alt="Noble Oak desktop view"
+              src="src/assets/images/holidazedesk.png"
+              alt="AnyCart desktop view"
               className="w-full md:w-1/2 object-contain mb-1 transform transition-transform duration-300 ease-in-out group-hover:scale-105"
             />
             <img
-              src="src/assets/images/nobleoakmobile.png"
-              alt="Noble Oak mobile view"
+              src="src/assets/images/holidazemobile.png"
+              alt="Holidaze mobile view"
               className="w-20 sm:w-36 lg:w-1/7 object-contain mb-1 transform transition-transform duration-300 ease-in-out group-hover:scale-105"
             />
           </div>
           <div className="flex flex-col justify-center items-center lg:p-10 xl:px-32 gap-3 mt-4">
-            <h2 className="font-barlow uppercase text-[24px] lg:text-[27px]">Noble Oak</h2>
+            <h2 className="font-barlow uppercase text-[24px] lg:text-[27px]">Holidaze</h2>
             <p className="text-sm lg:text-base ">
-                 Noble Oak is a fictional auction house created as a semester project at Noroff. Built using JavaScript, Sass, and Bootstrap, the website features functionality for listing auctions, placing bids, and managing user profile.
+                Holidaze is a fictional holiday venue booking website created as a project exam at Noroff. Built using React, Tailwind CSS, and JavaScript, the site includes functionality for searching venues, viewing details, making bookings, and managing user profiles.
             </p>
             <div className="flex flex-row gap-6">
                 <IconButton
-                url="https://github.com/aa096/SemesterProject2"
+                url="https://github.com/aa096/project-exam-2"
                 label="GitHub"
                 icon={faGithub}
                 />
                 <IconButton
-                url="https://nobleoakauction.netlify.app/"
+                url="https://holidaze-aa096.netlify.app/"
                 label="live site"
                 icon={faGlobe}
                 />
@@ -41,6 +41,6 @@ function NobleOakInfo() {
   );
 }
 
-export default NobleOakInfo;
+export default HolidazeInfo;
 
 
--- src/components/templates/projects.jsx
+++ src/components/templates/projects.jsx
@@ -1,10 +1,14 @@
 import AnyCartInfo from "./AnyCartInfo";
+import NobleOakInfo from "./NobleOakInfo";
+import HolidazeInfo from "./HolidazeInfo";
 
 function ProjectsTemplate() {
   return (
-    <div className="min-h-screen flex justify-center bg-primary bg-fixed bg-cover bg-no-repeat bg-portfolio p-5 text-primary">
+    <div className="min-h-screen flex flex-col justify-center bg-primary bg-fixed bg-cover bg-no-repeat bg-portfolio p-5 text-primary">
         <h1 className="text-white fixed top-8 right-4 sm:top-12 md:top-14 sl:top-16 lg:top-20 xl:top-24 z-10">By Aashild R. Lauvland</h1>
+        <HolidazeInfo />
         <AnyCartInfo />
+        <NobleOakInfo />
     </div>
   );
 }
