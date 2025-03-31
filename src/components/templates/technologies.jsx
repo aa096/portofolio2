a34b611374d2dd88c8f42d78dc726d88a327cbb3
aa096 <126397865+aa096@users.noreply.github.com>
2025-03-31 12:25:24 +0200

    technologies

--- src/components/templates/technologies.jsx
+++ src/components/templates/technologies.jsx
@@ -1,28 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBootstrap } from "@fortawesome/free-brands-svg-icons";
-import { faHtml5 } from "@fortawesome/free-brands-svg-icons";
-import { faCss } from "@fortawesome/free-brands-svg-icons";
-import { faJs } from "@fortawesome/free-brands-svg-icons"
-import { faSass } from "@fortawesome/free-brands-svg-icons";
-import { faReact } from "@fortawesome/free-brands-svg-icons";
+import { FaReact } from "react-icons/fa";
+import { SiCss3, SiJavascript, SiBootstrap, SiTailwindcss, SiSass, SiAdobephotoshop, SiFigma } from "react-icons/si";
+import { TiHtml5 } from "react-icons/ti";
 
 function Technologies() {
   return (
-      <section className="flex justify-center">
-        <div className="bg-white rounded-xl flex place-items-center z-10 flex-col p-10 lg:ml-[450px]  xl:ml-[550px] text-secondary max-w-[1200px] w-full">
-            <div className="flex flex-row gap-6 text-[40px]">
-            <FontAwesomeIcon icon={faHtml5} />
-            <FontAwesomeIcon icon={faCss} />
-            <FontAwesomeIcon icon={faJs} />
-            <FontAwesomeIcon icon={faBootstrap} />
-            <FontAwesomeIcon icon={faReact} />
-            <FontAwesomeIcon icon={faSass} />
-            </div>
-          </div>
-      </section>
+    <section className="flex justify-center">
+      <div className="bg-white rounded-xl flex place-items-center z-10 flex-col p-10 lg:ml-[450px] xl:ml-[550px] text-secondary max-w-[1200px] w-full">
+        <div className="flex flex-row gap-6 text-[40px]">
+          <TiHtml5 className="text-orange-500" title="HTML5" />
+          <SiCss3 className="text-blue-500" title="CSS3" />
+          <SiJavascript className="text-yellow-400" title="JavaScript" />
+          <SiBootstrap className="text-purple-600" title="Bootstrap" />
+          <SiTailwindcss className="text-cyan-500" title="Tailwind" />
+          <FaReact className="text-blue-400" title="React" />
+          <SiSass className="text-pink-400" title="Sass" />
+          <SiAdobephotoshop className="text-blue-900" title="Photoshop" />
+          <SiFigma className="text-blue-900" title="Figma" />
+
+
+        </div>
+      </div>
+    </section>
   );
 }
 
 export default Technologies;
-
-
